38eb75ebcf46650cb527be7bd1f9af8b41108af0
seunghoon <tmdgns4321@gmail.com>
2022-10-25 01:11:22 +0900

    서버 https 로 변경

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,15 +1,54 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { useRecoilState, selector } from 'recoil';
 import { loginState } from '../recoil/atom';
+import { FiChevronLeft } from 'react-icons/fi';
+import Home from './Home';
+import NaverMapAPI from '../components/NaverMap';
 
 const Detail = () => {
-  const pharmacy = axios
-    .get('http://tunamayo.shop/pharmacyList')
-    .then((res) => console.log(res));
-  console.log(pharmacy);
+  const { id } = useParams();
+  const [myLocation, setMyLocation] = useState('');
+
+  //약국 한 개 가져오기
+  // const pharmacy = axios
+  //   .get(`http://tunamayo.shop/pharmacyList?lat=${위도}&lng=${경도}`)
+  //   .then((res) => console.log(res));
+  // console.log(pharmacy);
   const [test, setTest] = useRecoilState(loginState);
-  return <div>{test}</div>;
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+    function success(pos) {
+      let crd = pos.coords;
+      setMyLocation({
+        lat: crd.latitude,
+        lng: crd.longitude,
+      });
+    }
+    function error() {
+      setMyLocation({ latitude: 37.4979517, longitude: 127.0276188 });
+    }
+  }, []);
+  console.log(myLocation);
+  return (
+    <div>
+      <Home />
+      <div className="w-full max-w-lg bg-slate-500 m-auto">
+        <div className=" sticky">
+          <FiChevronLeft size="30" />
+        </div>
+        <section>
+          <h1 className="text-2xl font-bold mb-4 mt-2.5">dutyName</h1>
+          <NaverMapAPI />
+        </section>
+        {test}
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
