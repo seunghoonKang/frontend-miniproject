046be03d89fc9122e02d1d002b2de3ca9938ff24
parkharoi <haha2000ab@gmail.com>
2022-10-24 11:17:42 +0900

    회원가입 하기 가라!

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,11 @@
 import React, { useRef } from 'react';
-import { useRecoilState } from 'recoil';
-import { loginState } from '../recoil/atom';
 import axios from 'axios'; // axios import 합니다.
-// import {
-//   // selector,
-//   // useRecoilState,
-//   // useRecoilValue,
-// } from 'recoil';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const idRef = useRef('');
   const pwRef = useRef('');
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -24,11 +19,22 @@ const Login = () => {
               className="px-5 py-3 mb-3 bg-gray-100 border-2 rounded-lg shadow-inner focus:outline-none focus:border-opacity-50 focus:border-green-600"
             />
             <input
-              type="password"
+              type="current-password"
               ref={pwRef}
               placeholder="PW "
               className="px-5 py-3 bg-gray-100 border-2 rounded-lg shadow-inner focus:outline-none focus:border-opacity-50 focus:border-green-600"
             />
+            <div className="flex justify-center my-3">
+              <div className="mx-3">
+                <p className="container mx-auto">회원이 아니신가요?</p>
+              </div>
+
+              <div className="mx-3 text-blue-600">
+                <button onClick={() => navigate('/register')}>
+                  회원가입하러 가기
+                </button>
+              </div>
+            </div>
             <button
               onClick={(e) => {
                 e.preventDefault();
@@ -36,7 +42,12 @@ const Login = () => {
                   userId: idRef.current.value,
                   password: pwRef.current.value,
                 };
-                axios.post('http://tunamayo.shop/users/login', login);
+                axios
+                  .post('http://tunamayo.shop/users/login', login)
+                  .then((res) => {
+                    console.log(res.data.token);
+                    localStorage.setItem('token', res.data.token);
+                  });
               }}
               className="px-5 py-3 mt-3 text-lg text-white bg-blue-400 rounded-lg focus:outline-none hover:opacity-90"
             >
@@ -51,35 +62,3 @@ const Login = () => {
 };
 
 export default Login;
-
-// function TextInput() {
-//   const [text, setText] = useRecoilState(textState);
-
-//   const onChange = (event) => {
-//     setText(event.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <input type="text" value={text} onChange={onChange} />
-//       <br />
-//       Echo: {text}
-//     </div>
-//   );
-// }
-// // textstate 아툼 키 = > text를 useRecoilState에 넣어줌으로서 찍힐떄마다 화면에 랜더링된다.
-
-// const charCountState = selector({
-//   key: 'charCountState', // unique ID (with respect to other atoms/selectors)
-//   get: ({ get }) => {
-//     const text = get(textState);
-
-//     return text.length;
-//   },
-// });
-
-// function CharacterCount() {
-//   const count = useRecoilValue(charCountState);
-
-//   return <>Character Count: {count}</>;
-// }
