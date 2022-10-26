2ea6e7413318da7cdf4ecc785e9842befcb05bf0
parkharoi <haha2000ab@gmail.com>
2022-10-26 15:51:02 +0900

    로그인 완료

--- src/pages/See.jsx
+++ src/pages/See.jsx
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
 const See = () => {
+  axios.get('https://chamchimayo.shop/users/?userId=userId').then((res) => {
+    console.log(res);
+    localStorage.setItem('token', res.data.token);
+  });
   return (
     <div>
       <div className="flex items-center justify-center h-screen bg-blue-400">
         <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center text-center>">
           <h3 className="text-3xl text-gray-800">회원정보 보기</h3>
           <form className="flex flex-col px-5 mt-5">
-            {axios
-              .get('https://chamchimayo.shop/users/:userId')
-              .then((res, req) => {
-                console.log(res.data.token);
-                localStorage.setItem('token', res.data.token);
-              })}
             <p className="px-5 py-3 mb-3 bg-gray-100 border-2 rounded-lg shadow-inner focus:outline-none focus:border-opacity-50 focus:border-green-600">
               ID
             </p>
